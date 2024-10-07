cd40349e35d6cce76d71ce72f6b2b3fe8ca2875f
asfakur-rahaman <asfakurrahaman24@gmail.com>
2024-10-07 23:02:30 +0600

    https://github.com/Asfakurrahaman/python-class-work.git

---
+++ work3.py
@@ -0,0 +1,3 @@
+def multiply_two_number(num1,num2):
+    return num1*num2
+print(multiply_two_number(15,10))
---
+++ work4.py
@@ -0,0 +1,5 @@
+
+
+def divide_two_number(num1,num2):
+    return num1/num2
+print(divide_two_number(15,10))
