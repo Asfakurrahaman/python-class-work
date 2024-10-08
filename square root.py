93375be74eb7f6bbf1becdd14bea57866e60eef8
asfakur-rahaman <asfakurrahaman24@gmail.com>
2024-10-08 16:14:49 +0600

    commit-m

--- square root.py
+++ square root.py
@@ -1,14 +1,3 @@
-
-def square_root(number, precision=0.00001):
-    if number < 0:
-        raise ValueError("Cannot compute square root of a negative number")
-
-    guess = number / 2 if number >= 1 else 1
-
-    while abs(guess * guess - number) > precision:
-        guess = (guess + number / guess) / 2
-
-    return guess
-num = 64
-result = square_root(num)
-print(f"The square root of {num} is approximately {result}")
+def square_root(number):
+    return number**(0.5)
+print(square_root(25))
