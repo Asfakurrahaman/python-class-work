68d5952f02707c79a1cd1abcf4ea3560c64d85f3
asfakur-rahaman <asfakurrahaman24@gmail.com>
2024-10-11 12:09:13 +0600

    commit-m

---
+++ differenceNumber.py
@@ -0,0 +1,5 @@
+def differenceNumber(num1,num2):
+    return num1 - num2
+
+Result = differenceNumber(20,15)
+print(Result)
---
+++ modulusNumber.py
@@ -0,0 +1,6 @@
+
+
+def remainder_two_number(num1,num2):
+    return num1%num2
+
+print(remainder_two_number(15,10))
